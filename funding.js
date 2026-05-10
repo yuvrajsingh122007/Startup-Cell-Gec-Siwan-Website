abc6a81d39a2de94e47cb77e61e81a1c12fa97d9
Yuvraj Singh <yuvraj.30122007@gmail.com>
2026-05-10 18:41:48 +0530

    Format Document

--- funding.js
+++ funding.js
@@ -77,7 +77,7 @@ function animateCounter(element, target) {
 
 // ========== SMOOTH SCROLL FOR ANCHOR LINKS ==========
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
